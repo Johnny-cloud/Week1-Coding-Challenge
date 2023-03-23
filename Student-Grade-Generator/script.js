ac886ef29afcbf80d2ce06e93479aa1ce618b9d7
John Kipruto <johnkipruto157@gmail.com>
2023-03-23 09:38:15 +0300

    Modified the student-grade-generator to notify if grade is less than 0 or above 100

--- Student-Grade-Generator/script.js
+++ Student-Grade-Generator/script.js
@@ -1,5 +1,6 @@
 //get grade text area
 let grade = document.querySelector('#grade');
+let gradeContainer = document.querySelector('#grade-container');
 
 grade.textContent = "Calculating ......";
 
@@ -24,18 +25,26 @@ form.addEventListener('submit', (e) => {
 
 //Check student marks against different conditions and print the necessary grades on the screen
 function setGrade(){
-    if(studentMarks > 79){
+    if(studentMarks > 100 || studentMarks < 0){
+        grade.textContent = "Invalid input";
+        gradeContainer.style.backgroundColor = "red";
+    }else if(studentMarks > 79){
         grade.textContent = "A";
-    
+        gradeContainer.style.backgroundColor = "green";
     }else if(studentMarks >= 60){
         grade.textContent = "B";
+        gradeContainer.style.backgroundColor = "green";
     }else if(studentMarks >= 49){
         grade.textContent = "C";
+        gradeContainer.style.backgroundColor = "green";
     }else if(studentMarks >= 40){
         grade.textContent = "D";
+        gradeContainer.style.backgroundColor = "green";
     }else if(studentMarks < 40){
         grade.textContent = "E"
+        gradeContainer.style.backgroundColor = "green";
     }else{
         grade.textContent = "-";
+        gradeContainer.style.backgroundColor = "green";
     }
 }
